73b624f47257d1cef70697e5aba375e33cabe80f
Jordy Alkema <jordy.alkema@home.nl>
2018-11-26 13:08:46 +0100

    Formatted

--- app.js
+++ app.js
@@ -6,26 +6,22 @@ const express = require("express");
 const app = express();
 
 const savePrompt = [
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter email adress...'
-    },
-    {
-        type: 'input',
-        name: 'pin',
-        message: 'Enter your lightwaverf pin code'
-    }
-]
+  {
+    type: "input",
+    name: "email",
+    message: "Enter email adress..."
+  },
+  {
+    type: "input",
+    name: "pin",
+    message: "Enter your lightwaverf pin code"
+  }
+];
 
-inquirer.prompt(savePrompt)
-    .then(saveparameters => api.saveSettings(saveparameters));
+inquirer
+  .prompt(savePrompt)
+  .then(saveparameters => api.saveSettings(saveparameters));
 
 app.listen(5000, () => {
-    console.log("App started on port 5000");
-})
-
-
-
-
-
+  console.log("App started on port 5000");
+});
