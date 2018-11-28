83d7cb61d0b7ffd8444b07917200b0498331c6ad
Jordy <jordy.alkema@home.nl>
2018-11-28 18:14:50 +0100

    Started work on deviceComponent logic

--- client/src/Components/deviceComponent/deviceComponent.js
+++ client/src/Components/deviceComponent/deviceComponent.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 class DeviceComponent extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <Typography component="p">{this.props.device.name}</Typography>;
+    if (this.props.device.status === "O") {
+      return (
+        <>
+          <Typography component="p">{this.props.device.name}</Typography>
+          <Button color="primary" variant="contained" />
+        </>
+      );
+    }
   }
 }
 
