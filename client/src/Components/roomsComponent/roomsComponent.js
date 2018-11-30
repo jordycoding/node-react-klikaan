4ec5edbffd66a4f4aa0a303e7834f6a079b604c6
Jordy Alkema <jordy.alkema@home.nl>
2018-11-30 19:28:02 +0100

    Added add icon to top bar rooms

--- client/src/Components/roomsComponent/roomsComponent.js
+++ client/src/Components/roomsComponent/roomsComponent.js
@@ -5,26 +5,42 @@ import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import RoomComponent from "../roomComponent/roomComponent";
+import AddIcon from "@material-ui/icons/Add";
+import IconButton from "@material-ui/core/IconButton";
 import "./roomsComponent.css";
-
 class RoomsComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { rooms: [] };
+    this.state = {
+      rooms: []
+    };
   }
   componentDidMount() {
     fetch("/getAllRooms")
       .then(res => res.json())
-      .then(resJson => this.setState({ rooms: resJson }));
+      .then(resJson =>
+        this.setState({
+          rooms: resJson
+        })
+      );
   }
   render() {
     return (
       <>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6" color="inherit">
+            <Typography
+              variant="h6"
+              color="inherit"
+              classes={{
+                root: "appText"
+              }}
+            >
               Ruimtes
             </Typography>
+            <IconButton color={"inherit"}>
+              <AddIcon />
+            </IconButton>
           </Toolbar>
         </AppBar>
         <div className="rooms">
