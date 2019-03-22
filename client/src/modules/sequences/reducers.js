81cb52816f876ab3079119e473ca2caa01f39e63
jordy <jordy.alkema@home.nl>
2019-03-22 20:29:22 +0100

    Added reducer and operations file to sequences duck

---
+++ client/src/modules/sequences/reducers.js
@@ -0,0 +1,23 @@
+import types from "./types"
+
+const initialState = {
+  sequencesLoading: false,
+  sequences: []
+};
+
+function sequencesReducer(state = initialState, action){
+  switch(action.type){
+    case types.SET_SEQUENCES_LOADING:
+      return {
+        ...state,
+        sequencesLoading: action.loading
+      }
+    case types.SET_SEQUENCES:
+      return {
+        ...state,
+        sequences: action.sequences
+      }
+    default:
+      return state
+  }
+}
