9a2efeab0b0efdfea7560cb0e2118ea33687a89b
jordy <jordy.alkema@home.nl>
2019-04-28 22:09:42 +0200

    Changed klikaanrequest class

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const router = require("./router");
 const morgan = require("morgan");
-const KlikaanRequest = require("./klikaanrequest")
+const KlikaanRequest = require("./klikaanrequest");
+const settings = require("./settings");
 
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
@@ -13,6 +14,5 @@ app.listen(5000, () => {
   console.log("App started on port 5000");
 });
 
-let request = new KlikaanRequest("test", "test@test.com")
-console.log(request.name)
-console.log(request.email)
+let request = new KlikaanRequest(settings.getMac(), settings.getEmail());
+request.executeCommand("!R7D1F0");
--- klikaanrequest.js
+++ klikaanrequest.js
@@ -2,11 +2,11 @@ const FormData = require("form-data");
 const fetch = require("node-fetch");
 
 class KlikaanRequest {
-  secret = "4beef6e4354fdeb384a8c5259e089623";
-  commandUrl = "https://api.trustsmartcloud.com/writerecord.php?";
-  username = "JSiPhone";
-  action = "I";
   constructor(name, email) {
+    this.secret = "4beef6e4354fdeb384a8c5259e089623";
+    this.commandUrl = "https://api.trustsmartcloud.com/writerecord.php?";
+    this.username = "JSiPhone";
+    this.action = "I";
     this.name = name;
     this.email = email;
   }
@@ -14,11 +14,11 @@ class KlikaanRequest {
     let body = new FormData();
     body.append("action", "I");
     body.append("username", "JSiPhone");
-    body.append("secret", secret);
+    body.append("secret", this.secret);
     body.append("name", this.name);
     body.append("email", this.email);
     body.append("commandstring", command);
-    return fetch("https://api.trustsmartcloud.com/writerecord.php?", {
+    fetch("https://api.trustsmartcloud.com/writerecord.php?", {
       method: "POST",
       body: body
     });
